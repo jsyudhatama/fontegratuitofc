40819b0d8fa01f062394915c256b2b056a89d75d
jsyudhatama <121849517+jsyudhatama@users.noreply.github.com>
2024-09-08 17:04:50 +0700

    Created Driver for Arduino Fake

---
+++ libraries/AP_InertialSensor/AP_InertialSensor_ArduSensor.h
@@ -0,0 +1,12 @@
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/SPIDevice.h>
+#include <AP_HAL/I2CDevice.h>
+
+#include "AP_InertialSensor.h"
+#include "AP_InertialSensor_Backend.h"
+
+
+
+class AP_InertialSensor_ArduSensor : public AP_InertialSensor_Backend {
+    
+}
